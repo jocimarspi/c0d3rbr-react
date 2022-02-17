e1c5a2d96703971ba15145b06979ace042f91be9
Jocimar B. A. Huss <jocimar.huss@db1.com.br>
2022-02-17 01:10:07 -0300

    Components with properties

---
+++ src/components/basics/WithParameter.jsx
@@ -0,0 +1,8 @@
+export default function WithParameter(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h3>{props.subtitle}</h3>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 
 import First from './components/basics/First';
+import WithParameter from './components/basics/WithParameter';
 
 import './index.css';
 
@@ -9,6 +10,8 @@ const tag = <strong>Olá React</strong>;
 ReactDOM.render(
   <div>
     <First />
+    <WithParameter title="Second component" subtitle="Very nice!" />
+    <WithParameter title="Third component" subtitle="Uhulll!" />
   </div>,
   document.getElementById("root")
 );
