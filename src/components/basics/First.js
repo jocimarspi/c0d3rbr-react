987412d8187bb19f4672ff782e8b737ab24c5cc3
Jocimar B. A. Huss <jocimar.huss@db1.com.br>
2022-02-17 00:59:40 -0300

    First Component

---
+++ src/components/basics/First.js
@@ -0,0 +1,11 @@
+export default function First() {
+
+  const msg = 'Welcome!';
+
+  return (
+    <div>
+      <h2>First Component</h2>
+      <p>{msg}</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom";
+
+import First from './components/basics/First';
+
 import './index.css';
 
 const tag = <strong>Olá React</strong>;
 
-ReactDOM.render(<div>{tag}</div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <First />
+  </div>,
+  document.getElementById("root")
+);
